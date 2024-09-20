53fd5c6ea1dfe3f936af580ee67e964e7dfbbf2d
Abdelaziz <mac@MacBook-Pro.local>
2024-09-21 00:06:50 +0100

    save point

--- src/check.c
+++ src/check.c
@@ -49,14 +49,59 @@ bool check_color(int *color)
 
 int check_ones(char *line)
 {
-	int j;
+	int i;
+	bool is_one;
 
-	j = 0;
-	while (line[j] && line[j] == '1')
-		j++;
-	if (line[j] != '\0')
-		ft_error("The map not srounded by walls1");
-	return j;
+	is_one = false;
+	i = 0;
+	while (line[i] && line[i] == SPACE)
+		i++;
+	while (line[i] && line[i] == '1')
+	{
+		is_one = true;
+		i++;
+	}
+	while (line[i] && line[i] == SPACE)
+		i++;
+	if (line[i] != '\0' || is_one == false)
+		ft_error("The map not srounded by walls 0");
+	return i;
+}
+
+void set_player_info(t_info *info, int x, int y)
+{
+	info->player_x = x * SIZE;
+	info->player_y = y * SIZE;
+	if (info->arr_map[y][x] == 'N')
+		info->player_angle = 270;
+	else if (info->arr_map[y][x] == 'E')
+		info->player_angle = 0;
+	else if (info->arr_map[y][x] == 'S')
+		info->player_angle = 90;
+	else
+		info->player_angle = 180;
+}
+
+void check_around(char **map, int x, int y, int height)
+{
+	// printf("(%d, %d) = |%c|\n", x, y, map[y][x]);
+	if (map[y][x] == '\0')
+		return;
+	if (map[y][x] == '1' && map[y + 1][x] == '\0')
+		return;
+	printf("(%d, %d) = |%c| -> y+1[%c] -> x+1[%c] \n", x, y, map[y][x], map[y + 1][x], map[y][x+1]);
+	if (map[y][x] == '0' && (map[y + 1][x] == SPACE || map[y + 1][x] == '\0'))
+	{
+		printf("error (%d, %d) = |%c|\n", x, y + 1, map[y][x + 1]);
+		ft_error("Map not srounded by walls 1");
+	}
+	if (map[y][x] == '0' && (map[y][x + 1] == SPACE || map[y][x + 1] == '\0'))
+	{
+		printf("error (%d, %d) = |%c|\n", x + 1, y, map[y][x + 1]);
+		ft_error("Map not srounded by walls 2");
+	}
+	check_around(map, x + 1, y, height);
+	check_around(map, 0, y + 1, height);
 }
 
 void check_map(t_info *info)
@@ -72,46 +117,34 @@ void check_map(t_info *info)
 	i = 1;
 	while (info->arr_map[i + 1])
 	{
-		len = j;
+		// len = j;
 		j = 0;
 		while (info->arr_map[i][j])
 		{
 			if (check_char(info->arr_map[i][j]) == false)
 				ft_error("invalid char");
-			// if (info->arr_map[i][j] != '1' && (len <= j || ft_strlen(info->arr_map[i + 1]) <= j))
-			// 	ft_error("the map not srounded by walls");
-			// if (info->arr_map[i][j] == '0')
+			// if (info->arr_map[i][j] == '0' && (info->arr_map[i + 1][j] == SPACE || info->arr_map[i + 1][j] == '\0'))
 			// {
-			// 	printf("len:%d\tj:%d\tlen +1 : %zu\n", len, j , ft_strlen(info->arr_map[i + 1]));
-			// 	if (len <= j || ft_strlen(info->arr_map[i + 1]) <= j)
-			// 		ft_error("the map not srounded by walls2");
-			// 	// printf("(%d, %d) |%c| |%c| |%c| |%c| |%c|\n", i, j , info->arr_map[i][j], info->arr_map[i - 1][j], info->arr_map[i + 1][j],info->arr_map[i][j - 1], info->arr_map[i][j + 1]);
-			// 	if (info->arr_map[i - 1][j] == SPACE || info->arr_map[i - 1][j] == '\0' || info->arr_map[i + 1][j] == SPACE || info->arr_map[i + 1][j] == '\0')
-			// 		ft_error("the map not srounded by walls3");
-			// 	if (info->arr_map[i][j - 1] == SPACE || info->arr_map[i][j - 1] == '\0' || info->arr_map[i][j + 1] == SPACE || info->arr_map[i][j + 1] == '\0')
-			// 		ft_error("the map not srounded by walls4");
+			// 	printf("error (%d, %d) = |%c|\n", j, i + 1, info->arr_map[i][j + 1]);
+			// 	ft_error("Map not srounded by walls 1");
+			// }
+			// if (info->arr_map[i][j] == '0' && (info->arr_map[i][j + 1] == SPACE || info->arr_map[i][j + 1] == '\0'))
+			// {
+			// 	printf("error (%d, %d) = |%c|\n", j + 1, i, info->arr_map[i][j + 1]);
+			// 	ft_error("Map not srounded by walls 2");
 			// }
 			if (info->arr_map[i][j] != '0' && info->arr_map[i][j] != '1' && info->arr_map[i][j] != SPACE)
 			{
-				info->player_x = j * SIZE;
-				info->player_y = i * SIZE;
-				if (info->arr_map[i][j] == 'N')
-					info->player_angle = 270;
-				else if (info->arr_map[i][j] == 'E')
-					info->player_angle = 0;
-				else if (info->arr_map[i][j] == 'S')
-					info->player_angle = 90;
-				else
-					info->player_angle = 180;
+				set_player_info(info, j, i);
 				count++;
 			}
 			j++;
 		}
-		if (info->arr_map[i][0] != '\0' && (info->arr_map[i][0] != '1' || info->arr_map[i][j - 1] != '1'))
-			ft_error("first or last char in line not 1");
 		i++;
 	}
 	check_ones(info->arr_map[i]);
 	if (count != 1)
 		ft_error("Problem in the Player numeber in the map");
+	check_around(info->arr_map, 0, 0, info->height);
+	// printf("|%c|\n", info->arr_map[0][13]);
 }
--- test.c
+++ test.c
@@ -64,49 +64,86 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+#include "libs/libft/libft.h"
 #include "libs/MLX42/include/MLX42/MLX42.h"
 #define WIDTH 256
 #define HEIGHT 256
 
 // Exit the program as failure.
-static void ft_error(void)
-{
-	fprintf(stderr, "%s", mlx_strerror(mlx_errno));
-	exit(EXIT_FAILURE);
-}
+// static void ft_error(void)
+// {
+// 	fprintf(stderr, "%s", mlx_strerror(mlx_errno));
+// 	exit(EXIT_FAILURE);
+// }
 
 // Print the window width and height.
-static void ft_hook(void* param)
-{
-	const mlx_t* mlx = param;
+// static void ft_hook(void* param)
+// {
+// 	const mlx_t* mlx = param;
 
-	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
+// 	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
+// }
+
+// int32_t	main(void)
+// {
+
+// 	// MLX allows you to define its core behaviour before startup.
+// 	mlx_set_setting(MLX_MAXIMIZED, true);
+// 	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "42Balls", true);
+// 	if (!mlx)
+// 		ft_error();
+
+// 	/* Do stuff */
+
+// 	// Create and display the image.
+// 	mlx_image_t* img = mlx_new_image(mlx, 256, 256);
+// 	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
+// 		ft_error();
+
+// 	// Even after the image is being displayed, we can still modify the buffer.
+// 	mlx_put_pixel(img, 0, 0, 0xFF0000FF);
+
+// 	// Register a hook and pass mlx as an optional param.
+// 	// NOTE: Do this before calling mlx_loop!
+// 	mlx_loop_hook(mlx, ft_hook, mlx);
+// 	mlx_loop(mlx);
+// 	mlx_terminate(mlx);
+// 	return (EXIT_SUCCESS);
+// }
+
+void ft_error(char *error_ms)
+{
+	write(STDERR_FILENO, error_ms, strlen(error_ms));
+	write(STDERR_FILENO, "\n", 1);
+	exit(1);
 }
 
-int32_t	main(void)
+int check_ones(char *line)
 {
+	int i;
+	bool is_one;
 
-	// MLX allows you to define its core behaviour before startup.
-	mlx_set_setting(MLX_MAXIMIZED, true);
-	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "42Balls", true);
-	if (!mlx)
-		ft_error();
+	is_one = false;
+	i = 0;
+	while (line[i] && line[i] == ' ')
+		i++;
+	while (line[i] && line[i] == '1')
+	{
+		is_one = true;
+		i++;
+	}
+	while (line[i] && line[i] == ' ')
+		i++;
+	if (line[i] != '\0' || is_one == false)
+		ft_error("The map not srounded by walls");
+	return i;
+}
 
-	/* Do stuff */
-
-	// Create and display the image.
-	mlx_image_t* img = mlx_new_image(mlx, 256, 256);
-	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
-		ft_error();
-
-	// Even after the image is being displayed, we can still modify the buffer.
-	mlx_put_pixel(img, 0, 0, 0xFF0000FF);
-
-	// Register a hook and pass mlx as an optional param.
-	// NOTE: Do this before calling mlx_loop!
-	mlx_loop_hook(mlx, ft_hook, mlx);
-	mlx_loop(mlx);
-	mlx_terminate(mlx);
-	return (EXIT_SUCCESS);
+int main()
+{
+	char *line = "11111111";
+	check_ones(line);
+	printf("Every thing is good \n");
 }
