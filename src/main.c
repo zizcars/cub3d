48cbbe9b86e1b0b2240aa6f1401f276884981c39
abdelaziz <achakkaf@c2r2p10.1337.ma>
2024-11-03 10:03:33 +0100

    done

--- src/main.c
+++ src/main.c
@@ -6,48 +6,11 @@
 /*   By: achakkaf <achakkaf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 15:13:37 by achakkaf          #+#    #+#             */
-/*   Updated: 2024/11/03 09:39:45 by achakkaf         ###   ########.fr       */
+/*   Updated: 2024/11/03 09:54:15 by achakkaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/utiles.h"
-
-// void print_info(t_info *info)
-// {
-// 	if (info)
-// 	{
-// 		printf("North path: %s\n", info->north_path);
-// 		printf("South path: %s\n", info->south_path);
-// 		printf("West path: %s\n", info->west_path);
-// 		printf("East path: %s\n", info->east_path);
-// 		if (info->c_color)
-// 			printf("Ceiling color: [%d, %d, %d]\n",
-// 		 info->c_color[0], info->c_color[1], info->c_color[2]);
-// 		else
-// 			printf("Ceiling color: (null)\n");
-// 		if (info->f_color)
-// 			printf("Floor color: [%d, %d, %d]\n",
-//  info->f_color[0], info->f_color[1], info->f_color[2]);
-// 		else
-// 			printf("Floor color: (null)\n");
-// 		printf("width: %d\t heigth: %d\n", info->width, info->height);
-// 		printf("Player:(%d,%d)\n", info->player_x, info->player_y);
-// 		if (info->map)
-// 		{
-// 			printf("MAP: \n");
-// 			for (int i = 0; info->map[i]; i++)
-// 				printf("|%s|\n", info->map[i]);
-// 		}
-// 		else
-// 			printf("map:(null)\n");
-// 	}
-// 	else
-// 		printf("info: (null)\n");
-// }
-// void leaks()
-// {
-// 	system("leaks -q cub3D");
-// }
 
 int	main(int ac, char **av)
 {
